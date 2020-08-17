7c26b4b06a35415ba09f02ef8164d7e29d53acd1
Giacomo Maretto <giacomomaretto@gmail.com>
2020-08-17 10:02:08 +0200

    feat eng

--- code/exploration.py
+++ code/exploration.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import ggplot
+#import ggplot
 
-df = pd.read_csv('C:\\Users\\giaco\\Documents\\ricerca lavoro\\Capgemini\\AIGenie_Capstone2.0\\movie.csv',sep=",",encoding="ISO-8859-1")
+df = pd.read_csv('movie.csv',sep=",",encoding="ISO-8859-1")
 
 df.head()
 plt.figure(figsize=(12,8))
